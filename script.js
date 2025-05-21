2cd7a932bcaf5fb7a06c42d396acc190ce4163bd
Jose Conca <vl.conca@gmail.com>
2025-05-21 18:56:43 +0100

    Mejoras generales, consola animada

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ function copyEmail() {
       .then(() => alert('Email copiado al portapapeles'));
   }
 
-  function copyTelf() {
-    const telfElement = document.getElementById('telf-text');
-    if (!telfElement) return;
-    const telf = telfElement.textContent;
-    navigator.clipboard.writeText(telf)
-      .then(() => alert('Teléfono copiado al portapapeles'));
+  // Expandir items 
+  document.querySelectorAll('.expandable .summary').forEach(el => {
+    el.addEventListener('click', () => {
+      el.parentElement.classList.toggle('open');
+    });
+  });
+
+
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    document.body.classList.add('light');
+    toggle.textContent = '🌘';
   }
-  
   // Theme toggle
   const toggle = document.getElementById('theme-toggle');
   toggle.addEventListener('click', () => {
@@ -21,10 +25,31 @@ function copyEmail() {
     toggle.textContent = document.body.classList.contains('light') ? '🌘' : '🌖';
   });
   
+
+
   // Loading console animation
+  let percent1 = 0;
+  const percentSpan1 = document.getElementById('percent1');
+  setTimeout(() => {
+    const interval = setInterval(() => {
+      percent1 = Math.min(percent1 + 1, 100);
+      percentSpan1.textContent = percent1 + '%';
+      if (percent1 === 100) clearInterval(interval);
+    }, 25); // Ajusta velocidad
+  }, 4500); // Espera
+
   let percent = 0;
-  const percentSpan = document.getElementById('percent');
-  setInterval(() => {
-    percent = Math.min(percent + 1, 100);
-    percentSpan.textContent = percent + '%';
-  }, 500);
+  const percentSpan = document.getElementById('percent2');
+  setTimeout(() => {
+    const interval = setInterval(() => {
+      percent = Math.min(percent + 1, 100);
+      percentSpan.textContent = percent + '%';
+      if (percent === 99) clearInterval(interval);
+    }, 100); // Ajusta velocidad
+  }, 10000); // Espera
+
+  setTimeout(() => {
+    const check = document.getElementById('ready-check');
+    check.classList.remove('hidden');
+    check.classList.add('visible');
+  }, 8000); // 2s después de "Web Dev Ready"
